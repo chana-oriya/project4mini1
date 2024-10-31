c0ff52fc81317ddb3a3a358b6d3f47a20019aee5
chana-oriya <chanafeuchtwanger.hilma.tech>
2024-10-31 10:16:20 +0200

    first change style

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import KeyBoard from './Components/KeyBoard';
 import ChangeColor from './Components/ChangeColor';
 
 function App() {
-  const [display, setDisplay] = useState("");
-  const [color, setColor] = useState("black");
-  
-  const addDisplay = (add) => {
-    //array of span
-   setDisplay((<span style={{color: color}}>{add}</span>));
+  const [display, setDisplay] = useState([{style: {color: "pink"}, char: "-"}]);
+  //const [color, setColor] = useState("black");
+  const [currStyle, setCurrStyle] = useState({color: "red"});
+
+  const addChar = (char) => {
+    
+    setDisplay((prevDisplay) => {
+        prevDisplay.push({style: currStyle, char: char});
+        return [...prevDisplay];
+      }
+    );
   }
+
+  const newStyle = (key, value) => {
+    console.log("in newStyle");
+    let newStyle = JSON.parse(JSON.stringify(currStyle)); //change to more correct way
+    newStyle[key] = value;
+    console.log(newStyle);
+    setCurrStyle(newStyle);
+  }
+
   const changeColor = (color) => {
-    setColor(color);
+    newStyle("color", color);
   }
 
   return (
     <>
       <h1>amazing key board!</h1>
-      <p style={{height: "50px"}}>{display}</p>
-      <KeyBoard addDisplay={addDisplay}/>
+      <p style={{height: "50px"}}>{
+        display.map((span) => (<span style={span.style}>{span.char}</span>))
+      }</p>
+      <KeyBoard addDisplay={addChar}/>
       <ChangeColor changeColor={changeColor}/>
     </>
   );
