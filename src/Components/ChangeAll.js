c6db88c9c6276008d393dda043072fc8aa35ce3a
chana-oriya <chanafeuchtwanger.hilma.tech>
2024-10-31 12:36:29 +0200

    change on all color and size

--- src/Components/ChangeAll.js
+++ src/Components/ChangeAll.js
@@ -1,9 +1,19 @@
 function ChangeAll(props){
     
+    const colorsToChange = ['red', 'black', 'blue'];
+    const sizeToChange = ['16', '20', '24'];
+
     return (
         <div>
+            <h3>change all!</h3>
             <button onClick={() => props.changeAll('case', 'upper')}>Upper Case</button>
             <button onClick={() => props.changeAll('case', 'lower')}>Lower Case</button>
+            <div>
+                {colorsToChange.map( (item) => <button onClick={() => props.changeAll('color', item)}>{item}</button>)}         
+            </div>
+            <div>
+                {sizeToChange.map( (item) => <button onClick={() => props.changeAll('fontSize', item+"px")}>{item}</button>)}         
+            </div>
         </div>
     )
 }
